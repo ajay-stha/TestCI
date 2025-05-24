97e079682863b7c70f387823b2da8f696dfd4021
ajay-stha <ajay.shrestha54@gmail.com>
2025-05-24 20:56:36 +0545

    Refactor BaseViewModel: Remove INotifyPropertyChanged
    Removed `INotifyPropertyChanged` implementation and associated
    members from `BaseViewModel`. The `_Title` field was replaced
    with a read-only `Title` property that uses conditional
    compilation for different build configurations. Unused
    namespace directives were also removed.

--- MyCLIWpfApp.Wpf/ViewModels/BaseViewModel.cs
+++ MyCLIWpfApp.Wpf/ViewModels/BaseViewModel.cs
@@ -1,39 +1,16 @@
-﻿using System.ComponentModel;
-using System.Runtime.CompilerServices;
-
-namespace MyCLIWpfApp.Wpf.ViewModels
+﻿namespace MyCLIWpfApp.Wpf.ViewModels
 {
-    public class BaseViewModel : INotifyPropertyChanged
+    public class BaseViewModel
     {
-        public string? _Title =
+        public string Title
+        {
+            get =>
 #if RELEASE
     "Main Window";
 #else
     "Main Window QA";
 #endif
-
-
-        public string? Title
-        {
-            get => _Title;
-            set => SetProperty(ref _Title, value);
         }
 
-        #region INotifyPropertyChanged Implementation
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
-            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-
-        protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = "")
-        {
-            if (EqualityComparer<T>.Default.Equals(field, value))
-                return false;
-
-            field = value;
-            OnPropertyChanged(propertyName);
-            return true;
-        }
-        #endregion
     }
 }
